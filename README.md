05902ef400e6ff48412c798fe97112d6767f9aa8
Dogo <dogosolutions@gmail.com>
2024-03-09 18:05:06 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ip-viewer-app
- An Electron app for IP Viewer
+# IP Viewer Project with Electron
+
+This project utilizes Electron to create an IP address viewer application. The application loads a web page with IP address information from [ip.doso.es](https://ip.doso.es/).
+
+
+## Compile
+
+```electron-packager . ipviewer --platform=win32 --arch=x64 --icon=assets/icon.ico --asar```
---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        icon: path.join(__dirname, 'assets/icon.png')
+    });
+    
+    win.loadURL('https://ip.doso.es/');
+    win.setMenu(null);
+}
+
+app.whenReady().then(createWindow);
